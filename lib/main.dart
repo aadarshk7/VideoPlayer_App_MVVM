00c8170c4166f783283e0756700d7f0f27c4a53e
Aadarsh Kunwar <106459626+aadarshk7@users.noreply.github.com>
2024-11-06 15:59:15 +0545

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'views/video_list_page.dart';
+import 'package:geolocator/geolocator.dart'; // For location tracking
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,9 +16,16 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Video Player',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black),
+          bodyMedium: TextStyle(color: Colors.black54),
+        ),
+        buttonTheme: const ButtonThemeData(
+          buttonColor: Colors.blue,
+          textTheme: ButtonTextTheme.primary,
+        ),
       ),
       home: VideoListPage(),
-      // home: VideoListPage(),
     );
   }
 }
